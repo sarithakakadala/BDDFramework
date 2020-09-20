da1a780819efeee8cdebc774c38dc844e0d8b94d
sarithakakadala <sarithakakadala@gmail.com>
2020-09-20 19:49:04 +0530

    first commit

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -5,15 +5,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 
-	@RunWith(Cucumber.class)
-	@CucumberOptions(
-			features = "F:\\HybridFramework\\BDDFrameWork\\src\\main\\java\\Features\\taggedhooks.feature", //the path of the feature files
-			glue={"stepDefinitions"}, //the path of the step definition files
-			format= {"pretty","html:test-output","json:json_output/cucumber.json","junit:junit_otput/cucumber.xml"},//result file
-			dryRun=false, //step definition mapping if its value is true
-			monochrome=true //display cosole output in readable format
-			//tags = {"~@SmokeTest","@RegressionTest"}
-			)
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "F:\\HybridFramework\\BDDFrameWork\\src\\main\\java\\Features\\taggedhooks.feature", //the path of the feature files
+		glue={"stepDefinitions"}, //the path of the step definition files
+		format= {"pretty","html:test-output","json:json_output/cucumber.json","junit:junit_otput/cucumber.xml"},//result file
+		dryRun=false, //step definition mapping if its value is true
+		monochrome=true //display cosole output in readable format
+		//tags = {"~@SmokeTest","@RegressionTest"}
+		)
 public class TestRunner {
 
 }
